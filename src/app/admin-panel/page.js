071870e0840338636a58361b94068ec3174cb873
Vivekkumarv123 <164018649+Vivekkumarv123@users.noreply.github.com>
2025-03-24 06:52:22 +0530

    Add files via upload

---
+++ src/app/admin-panel/page.js
@@ -0,0 +1,12 @@
+import AdminPanel from '@/components/Admin/AdminPanel'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <AdminPanel/>
+    </div>
+  )
+}
+
+export default page
