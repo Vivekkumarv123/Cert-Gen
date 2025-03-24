3e60855e914acdddfe1abb37e4de8b011c22d3ae
Vivekkumarv123 <164018649+Vivekkumarv123@users.noreply.github.com>
2025-03-24 06:48:57 +0530

    Create page.js

---
+++ src/app/admin-panel/create-certificate/page.js
@@ -0,0 +1,12 @@
+import CreateCertificate from '@/components/Certificate/CreateCertificate'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <CreateCertificate/>
+    </div>
+  )
+}
+
+export default page
